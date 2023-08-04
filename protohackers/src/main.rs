f2a416b75bea300f9480827f27d979b0ade8fa7e
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-08-03 23:38:52 -0500

    Add solution for Problem 2, Prime Time.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- protohackers/src/main.rs
+++ protohackers/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use protohackers::solutions::echo_server;
+use protohackers::solutions::prime_time;
 use tracing::info;
 
 
@@ -19,6 +20,9 @@ pub struct Args {
 pub async fn start_echo_server() {
     echo_server::EchoServer { port: 12000 }.run().await.unwrap();
 }
+pub async fn start_prime_time() {
+    prime_time::PrimeTime { port: 12001 }.run().await.unwrap();
+}
 
 
 
@@ -30,7 +34,8 @@ async fn main() {
     match args.solution {
         None => {
             tokio::join!(
-                start_echo_server()
+                start_echo_server(),
+                start_prime_time()
             );
         },
         Some(x) => {
@@ -38,6 +43,9 @@ async fn main() {
                 0 => {
                     _ = start_echo_server().await;
                 },
+                1 => {
+                    _ = start_prime_time().await;
+                }
                 _ => {
                     unimplemented!("unknown solution id")
                 }
---
+++ protohackers/src/solutions/prime_time.rs
@@ -0,0 +1,163 @@
+use std::net::SocketAddr;
+use tracing::{info, error, warn, trace};
+
+use tokio::{net::{TcpListener, TcpStream}, io::{AsyncReadExt, AsyncWriteExt}};
+use std::io::BufReader;
+
+
+#[derive(Debug)]
+pub struct PrimeTime {
+    pub port: u16,
+}
+
+pub mod data {
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+    pub struct IsPrimeRequest {
+        pub method: String,
+        pub number: f64
+    }
+
+    #[derive(Debug, serde::Serialize, Clone)]
+    pub struct IsPrimeResponse {
+        pub method: String,
+        pub prime: bool
+    }
+
+}
+
+
+
+
+impl Default for PrimeTime {
+    fn default() -> Self {
+        Self {
+            port: 12001
+        }
+    }
+}
+
+pub const TOLERANCE: f64 = 1e-16;
+
+impl PrimeTime {
+    pub fn loopback_addr(&self) -> SocketAddr {
+        ([0; 8], self.port).into()
+    }
+
+    pub fn is_prime_f64(number: f64) -> bool {
+        if (number.floor() - number).abs() > TOLERANCE {
+            return false;
+        }
+        Self::is_prime(number.floor() as u64)
+    }
+
+    pub fn is_prime(number: u64) -> bool {
+        let start = 2;
+        let end = ((number as f64).sqrt().ceil() + 1.0) as u64;
+        (start..=end)
+        .all(|divisor| number % divisor != 0)
+    }
+
+    pub async fn run(self) -> Result<(), Box<dyn std::error::Error>> {
+        
+        let listener = TcpListener::bind(self.loopback_addr()).await?;
+        info!("PrimeTime listening on {}", self.loopback_addr());
+
+        loop {
+            let (stream, addr) = listener.accept().await?;
+            tokio::task::spawn(async move {
+                Self::handle_connection(stream, addr).await;
+            });
+        }
+    }
+
+    pub async fn handle_connection(
+        stream: TcpStream,
+        client_addr: SocketAddr,
+    ) {
+        let (mut read_half, mut write_half) = stream.into_split();
+        info!("Accepted connection from {}", client_addr);
+
+        let mut buf = [0u8; 1024];
+
+        'conn: loop {
+            let Ok(bytes_read) = read_half.read(&mut buf).await else {
+                error!("Failed to read from read half");
+                break;
+            };
+
+            if bytes_read == 0 {
+                trace!("Received EOF from client {}. Terminating connection.", client_addr);
+                break;
+            }
+
+            let mut current_offset = 0;
+            loop {
+                if current_offset >= bytes_read {
+                    break;
+                }
+                let mut buf_reader = BufReader::new(&buf[current_offset..bytes_read]);
+                match serde_json::from_reader::<_, data::IsPrimeRequest>(&mut buf_reader) {
+                    Ok(request) => {
+
+                        if &request.method != "isPrime" {
+                            warn!("Method is not `isPrime`. Treating this as a malformed request...");
+                            if let Err(write_err) = write_half.write(&[1, 2]).await {
+                                error!("failed to send malformed response: {}", write_err);
+                            }
+                            break 'conn;
+                        }
+
+                        let size = serde_json::to_vec(&request).unwrap().len();
+                        current_offset += size;
+                        if current_offset < bytes_read {
+                            if buf[current_offset] == b'\n' {
+                                current_offset += 1;
+                            }
+                            else {
+                                warn!("Not newline terminated. Treating this as a malformed request... :sus:");
+                                if let Err(write_err) = write_half.write(&[1, 2]).await {
+                                    error!("failed to send malformed response: {}", write_err);
+                                }
+                                break 'conn;
+                            }
+                        }
+
+                        let is_prime = Self::is_prime_f64(request.number);
+                        let response = data::IsPrimeResponse {
+                            prime: is_prime,
+                            method: "isPrime".to_string()
+                        };
+
+                        let response_bytes = serde_json::to_vec(&response).unwrap();
+                        if let Err(err) = write_half.write_all(&response_bytes).await {
+                            error!("Failed to write conforming response: {}", err);
+                        }
+                    },
+                    Err(err) => {
+                        error!("Received unparseable data from client. Sending malformed data and terminating connection... {}", err);
+                        if bytes_read == buf.len() {
+                            warn!("We filled the entire buffer up. Might be an incomplete frame?");
+                        }
+                        // Some random data, who cares, its malformed anyway.
+                        if let Err(write_err) = write_half.write(&[1, 2]).await {
+                            error!("failed to send malformed response: {}", write_err);
+                        }
+                        break 'conn;
+                    }
+                }
+            }
+        }
+
+        let mut stream = read_half.reunite(write_half).expect("failed to reunite.");
+        if let Err(err) = stream.flush().await {
+            error!("failed to flush stream for {}: {}", client_addr, err);
+        }
+        if let Err(err) = stream.shutdown().await {
+            error!("failed to shutdown stream: {}", err);
+        } else {
+            info!("Closed connection from {}", client_addr);
+        }
+    }
+
+}
