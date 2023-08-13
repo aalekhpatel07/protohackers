cfbf28959bb260ae68e83a37172be99a9352bde9
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-08-13 00:02:05 -0500

    Rustfmt fixes.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- protohackers/unusual-database-program/src/main.rs
+++ protohackers/unusual-database-program/src/main.rs
@@ -1,16 +1,14 @@
 use std::net::SocketAddr;
 use tokio::net::UdpSocket;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
-use tokio::sync::oneshot::{Sender, channel};
-use tracing::{info, debug, trace, error};
+use tokio::sync::oneshot::{channel, Sender};
+use tracing::{debug, error, info, trace};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
 
 #[derive(Debug, Default, Clone)]
 pub struct KeyValueStore {
     store: std::collections::HashMap<String, String>,
 }
-
 
 impl KeyValueStore {
     pub fn insert(&mut self, key: &str, value: &str) -> Option<String> {
@@ -18,35 +16,23 @@ impl KeyValueStore {
         None
     }
     pub fn retrieve(&self, key: &str) -> Option<String> {
-        self
-        .store
-        .get(key)
-        .map(|value| format!("{}={}", key, value))
+        self.store
+            .get(key)
+            .map(|value| format!("{}={}", key, value))
     }
 }
 
-
 #[derive(Debug)]
 pub enum Operation {
-    Insert {
-        key: String,
-        value: String,
-    },
-    Retrieve {
-        key: String
-    }
+    Insert { key: String, value: String },
+    Retrieve { key: String },
 }
 
 impl Operation {
     pub fn is_forbidden(&self) -> bool {
         match self {
-            Self::Insert {
-                key, 
-                ..
-            } => {
-                key == "version"
-            },
-            _ => false
+            Self::Insert { key, .. } => key == "version",
+            _ => false,
         }
     }
 }
@@ -56,35 +42,30 @@ impl From<String> for Operation {
         match v.find('=') {
             Some(index) => {
                 let (key, value) = v.split_at(index);
-                Operation::Insert { key: key.to_string(), value: value.chars().skip(1).collect() }
-            },
-            None => {
-                Operation::Retrieve { key: v }
+                Operation::Insert {
+                    key: key.to_string(),
+                    value: value.chars().skip(1).collect(),
+                }
             }
+            None => Operation::Retrieve { key: v },
         }
     }
 }
 
-
 pub type Response = Option<String>;
 pub type Request = (Operation, Sender<Response>);
 
-
 async fn handle_operations(
     store: std::sync::Arc<std::sync::Mutex<KeyValueStore>>,
-    mut operations_rx: UnboundedReceiver<Request>
+    mut operations_rx: UnboundedReceiver<Request>,
 ) {
     loop {
         if let Some((operation, response_tx)) = operations_rx.recv().await {
             let mut guard = store.lock().unwrap();
             let op_debug = format!("{:#?}", operation);
             let response = match operation {
-                Operation::Insert { key, value } => {
-                    guard.insert(key.as_str(), value.as_str())
-                },
-                Operation::Retrieve { key } => {
-                    guard.retrieve(&key)
-                }
+                Operation::Insert { key, value } => guard.insert(key.as_str(), value.as_str()),
+                Operation::Retrieve { key } => guard.retrieve(&key),
             };
             info!("Sending result of processing {}: {:?}", op_debug, response);
             response_tx.send(response).unwrap();
@@ -94,12 +75,10 @@ async fn handle_operations(
 
 async fn run_server(
     operations_tx: UnboundedSender<Request>,
-    port: u16
+    port: u16,
 ) -> Result<(), Box<dyn std::error::Error>> {
-
     let addr: SocketAddr = ([0; 8], port).into();
     let socket = std::sync::Arc::new(UdpSocket::bind(addr).await?);
-
 
     let mut buf = [0; 1024];
     let socket_recv = socket.clone();
@@ -124,17 +103,20 @@ async fn run_server(
             let socket_send = socket.clone();
             let (tx, rx) = channel();
 
-            operations_tx.send((operation, tx)).unwrap();            
+            operations_tx.send((operation, tx)).unwrap();
 
             tokio::task::spawn(async move {
                 match rx.await {
                     Ok(Some(maybe_response)) => {
                         debug!(peer = %remote_addr, "Received result of processing an operation for {}", remote_addr);
-                        socket_send.send_to(maybe_response.as_bytes(), remote_addr).await.unwrap();
-                    },
+                        socket_send
+                            .send_to(maybe_response.as_bytes(), remote_addr)
+                            .await
+                            .unwrap();
+                    }
                     Ok(None) => {
                         debug!(peer = %remote_addr, "Request was processed but we chose to not send a response back.");
-                    },
+                    }
                     Err(err) => {
                         error!(peer = %remote_addr, "Error waiting for a response for the request of {}\n {}", remote_addr, err);
                     }
@@ -143,7 +125,6 @@ async fn run_server(
         }
     }
 }
-
 
 #[tokio::main]
 
@@ -167,11 +148,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let operations_handle = tokio::task::spawn(handle_operations(store, operations_rx));
 
-    _ = tokio::join!(
-        operations_handle,
-        run_server(operations_tx, 12002)
-    );
+    _ = tokio::join!(operations_handle, run_server(operations_tx, 12002));
 
     Ok(())
-
 }
