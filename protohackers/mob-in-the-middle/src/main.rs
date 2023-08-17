a07783050c310c185aca25a944be4110d165c001
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-08-16 22:34:01 -0500

    Rustfmt fixes.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- protohackers/mob-in-the-middle/src/main.rs
+++ protohackers/mob-in-the-middle/src/main.rs
@@ -1,14 +1,18 @@
-use std::net::{SocketAddr};
+use std::net::SocketAddr;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+    select,
+    sync::mpsc::{UnboundedReceiver, UnboundedSender},
+};
 use tracing::{debug, trace};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tokio::{sync::mpsc::{UnboundedReceiver, UnboundedSender}, net::{TcpListener, TcpStream}, select, io::{BufReader, AsyncBufReadExt, AsyncWriteExt}};
 
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref BOGUSCOIN_REGEX: regex::Regex = regex::Regex::new(r#"\b7\w{26,35}\b"#).unwrap();
 }
-
 
 #[derive(Debug)]
 struct ProxyBudgetChat {
@@ -18,7 +22,6 @@ struct ProxyBudgetChat {
 }
 
 impl ProxyBudgetChat {
-
     const BOGUSCOIN_ADDRESS: &'static str = "7YWHMfk9JZe0LM0g1ZauHuiSxhI";
 
     #[tracing::instrument]
@@ -30,7 +33,9 @@ impl ProxyBudgetChat {
         let (prefix_to_keep, message_to_transform) = msg.split_at_mut(skip_chars as usize);
         trace!("transforming message cleaned: {:?}", message_to_transform);
 
-        let replaced = BOGUSCOIN_REGEX.replace_all(message_to_transform, Self::BOGUSCOIN_ADDRESS).to_string();
+        let replaced = BOGUSCOIN_REGEX
+            .replace_all(message_to_transform, Self::BOGUSCOIN_ADDRESS)
+            .to_string();
         trace!("transformed cleaned: {}", replaced);
 
         let joined = format!("{}{}", prefix_to_keep, replaced);
@@ -44,7 +49,6 @@ impl ProxyBudgetChat {
     }
 
     pub async fn run(&mut self) -> Result<()> {
-
         let stream = TcpStream::connect("chat.protohackers.com:16963").await?;
         let (reader_half, mut writer_half) = stream.into_split();
 
@@ -86,9 +90,8 @@ impl ProxyBudgetChat {
     }
 }
 
-
-pub type Result<T, E = Box<dyn std::error::Error + Send + Sync + 'static>> = core::result::Result<T, E>;
-
+pub type Result<T, E = Box<dyn std::error::Error + Send + Sync + 'static>> =
+    core::result::Result<T, E>;
 
 pub async fn run_server(port: u16) -> Result<()> {
     let addr: SocketAddr = ([0; 8], port).into();
@@ -97,15 +100,12 @@ pub async fn run_server(port: u16) -> Result<()> {
         let (socket, _) = listener.accept().await.unwrap();
         tokio::task::spawn(async move { handle_client(socket).await });
     }
-
 }
 
-pub async fn handle_client(
-    socket: TcpStream,
-) -> Result<()> {
-
+pub async fn handle_client(socket: TcpStream) -> Result<()> {
     let (messages_from_client_tx, messages_from_client_rx) = tokio::sync::mpsc::unbounded_channel();
-    let (messages_to_client_tx, mut messages_to_client_rx) = tokio::sync::mpsc::unbounded_channel::<String>();
+    let (messages_to_client_tx, mut messages_to_client_rx) =
+        tokio::sync::mpsc::unbounded_channel::<String>();
 
     let (reader_half, mut writer_half) = socket.into_split();
     let reader = BufReader::new(reader_half);
@@ -142,8 +142,8 @@ pub async fn handle_client(
         }
     });
 
-    let mut proxy = ProxyBudgetChat { 
-        outbound_message_rx: messages_from_client_rx, 
+    let mut proxy = ProxyBudgetChat {
+        outbound_message_rx: messages_from_client_rx,
         inbound_message_tx: messages_to_client_tx,
         name_ceremony_complete: false,
     };
@@ -152,8 +152,6 @@ pub async fn handle_client(
 
     Ok(())
 }
-
-
 
 #[tokio::main]
 async fn main() {
