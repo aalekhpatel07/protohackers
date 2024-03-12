16909a1729b3124cce665930183da04ff94a226e
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2024-03-12 00:36:13 -0500

    Setup sqlalchemy and sqlite for state machine
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- python-solutions/solutions/speed_daemon/state.py
+++ python-solutions/solutions/speed_daemon/state.py
@@ -1,59 +1,76 @@
 import dataclasses
 import typing
-from collections import defaultdict
 import structlog
-
+from .models import (
+    session_scope,
+    Road,
+    TicketDispatcher,
+    Camera,
+    RoadDispatcher,
+    SpeedObservation,
+)
 
 logger = structlog.get_logger(__name__)
 
 
-# type ID = typing.Tuple[str, int]
+class SpeedDaemon:
 
+    def __init__(self, session):
+        self.session = session
 
-@dataclasses.dataclass
-class Camera[ID]:
-    id: ID
-    road: int
-    mile: int
-    limit: float
+    def get_dispatchers(self) -> typing.List[TicketDispatcher]:
+        return list(TicketDispatcher.all_dispatchers(self.session))
 
+    def add_dispatcher(self, client: str, roads: typing.List[int]):
+        dispatcher, added = TicketDispatcher.get_or_create(self.session, client=client)
+        if added:
+            logger.info("Added Ticket Dispatcher: ", client=client, roads=roads)
+        road_instances: typing.List[Road] = []
+        for road in roads:
+            road_ins, added = Road.get_or_create(self.session, id=road)
+            if added:
+                logger.info("Added Road", client=client, road=road_ins.id)
+            road_instances.append(road_ins)
+        self.session.add_all(
+            [
+                RoadDispatcher(road_id=road.id, ticket_dispatcher_id=dispatcher.id)
+                for road in road_instances
+            ]
+        )
 
-@dataclasses.dataclass
-class TicketDispatcher[ID]:
-    id: ID
-    roads: typing.List[int] = dataclasses.field(default_factory=list)
+    def remove_dispatcher(self, client: str):
+        entry = self.session.query(TicketDispatcher).filter_by(client=client)
+        if entry.first() is not None:
+            logger.info("Removing Ticket Dispatcher: ", client=client, dispatcher_id=entry.first().id)
+        entry.delete()
+        # if dispatcher_id in self.dispatchers:
+        #     prev = self.dispatchers.pop(dispatcher_id)
+        #     logger.info(f"Removed dispatcher", id=prev.id, value=prev)
+        # else:
+        #     logger.warn(f"Dispatcher not found", id=dispatcher_id)
 
+    def add_camera(self, client: str, road: int, mile: int, limit: float):
+        road, added = Road.get_or_create(self.session, id=road)
+        if added:
+            logger.info(f"Added Road", id=road.id, client=client)
+        camera, added = Camera.get_or_create(self.session, client=client, limit=limit, mile=mile, road=road)
 
-@dataclasses.dataclass
-class SpeedDaemon[ID]:
-    dispatchers: typing.Dict[ID, TicketDispatcher] = dataclasses.field(default_factory=dict)
-    cameras: typing.Dict[ID, Camera] = dataclasses.field(default_factory=dict)
+        if added:
+            logger.info(f"Added Camera", id=camera.id, client=camera.client, limit=limit, mile=mile, road=road.id)
 
-    def add_dispatcher(self, dispatcher_id: ID, roads: typing.List[int]):
-        self.dispatchers[dispatcher_id] = TicketDispatcher(dispatcher_id, roads)
-        logger.info(f"Added dispatcher", id=dispatcher_id, value=self.dispatchers[dispatcher_id])
+    def remove_camera(self, client: str):
+        self.session.query(Camera).filter_by(client=client).delete()
+        # if camera_id in self.cameras:
+        #     prev = self.cameras.pop(camera_id)
+        #     logger.info(f"Removed camera", id=prev.id, value=prev)
+        # else:
+        #     logger.warn(f"Camera not found", id=camera_id)
 
-    def remove_dispatcher(self, dispatcher_id: ID):
-        if dispatcher_id in self.dispatchers:
-            prev = self.dispatchers.pop(dispatcher_id)
-            logger.info(f"Removed dispatcher", id=prev.id, value=prev)
-        else:
-            logger.warn(f"Dispatcher not found", id=dispatcher_id)
-
-    def add_camera(self, camera_id: ID, road: int, mile: int, limit: float):
-        self.cameras[camera_id] = Camera(camera_id, road, mile, limit)
-        logger.info(f"Added camera", id=camera_id, value=self.cameras[camera_id])
-
-    def remove_camera(self, camera_id: ID):
-        if camera_id in self.cameras:
-            prev = self.cameras.pop(camera_id)
-            logger.info(f"Removed camera", id=prev.id, value=prev)
-        else:
-            logger.warn(f"Camera not found", id=camera_id)
-
-    def record_car(self, plate: str, timestamp: int, camera_id: ID):
-        self.cars_by_camera[camera_id][timestamp] = plate
-        logger.info("Recorded car", camera_id=camera_id, car_timestamp=timestamp, plate=plate)
+    def record_car(self, plate: str, timestamp: int, client: str):
+        camera = self.session.query(Camera).filter_by(client=client).one()
+        instance, created = SpeedObservation.get_or_create(self.session, plate=plate, timestamp=timestamp, camera_id=camera.id)
+        if created:
+            logger.info("Added Observation", plate=plate, timestamp=timestamp, client=client, camera_id=camera.id)
 
     def check_for_tickets(self):
 
--- python-solutions/tests/speed_daemon.py
+++ python-solutions/tests/speed_daemon.py
@@ -1,24 +1,44 @@
 import pytest
-import asyncio
-from solutions.speed_daemon.messages import (
-    Message,
-    Plate,
-)
 from solutions.speed_daemon.state import (
     SpeedDaemon,
     TicketDispatcher,
-    Camera
+    Camera,
+    SpeedObservation,
+)
+from solutions.speed_daemon.models import (
+    session_scope,
+    Session,
 )
 
 
-@pytest.mark.asyncio
-async def test_single_car():
-    state = SpeedDaemon[int]()
-    state.add_camera(1, 9822, 8100, 100)
-    state.add_camera(2, 9822, 8090, 100)
-    state.record_car('ZM78NBG', 366166, 1)
-    state.add_dispatcher(1, [49822])
-    state.record_car('ZM78NBG', 365866, 2)
+@pytest.fixture
+def session():
+    with session_scope() as sess:
+        yield sess
 
-    # state.add_camera(2, 10, 3, 12.0)
-    pass
+
+def test_single_car(session):
+    state = SpeedDaemon(session)
+
+    state.add_camera("1", 9822, 8100, 100)
+    state.add_camera("2", 9822, 8090, 100)
+    state.record_car('ZM78NBG', 366166, "1")
+    state.add_dispatcher("3", [49822])
+    state.record_car('ZM78NBG', 365866, "1")
+
+    dispatchers = state.get_dispatchers()
+    assert len(dispatchers) == 1
+    assert dispatchers[0].client == "3"
+    assert dispatchers[0].roads[0].id == 49822
+
+    assert session.query(Camera).count() == 2
+    assert session.query(Camera.mile).order_by(Camera.client).all() == [(8100,), (8090,)]
+    assert session.query(Camera.limit).order_by(Camera.client).all() == [(100,), (100,)]
+
+    assert session.query(SpeedObservation).count() == 2
+
+    state.remove_camera("1")
+    assert session.query(Camera).count() == 1
+
+    state.remove_dispatcher("3")
+    assert session.query(TicketDispatcher).count() == 0
